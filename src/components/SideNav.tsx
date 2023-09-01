bb4b1a32eeb90f09b99df6d3ad857997e399d561
1willcobb <cobb.will@gmail.com>
2023-09-01 14:32:48 -0500

    side nav JS functional

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
+import '../App.css'; // Import your stylesheet where CSS variables are defined
 
 const SideNav = () => {
+  const [sideBarEnabled, setSideBarEnabled] = useState(false);
+
+  const toggleSideBar = () => {
+    setSideBarEnabled(!sideBarEnabled);
+
+    const root = document.documentElement; // Get the root element
+    root.style.setProperty('--side-bar-size', sideBarEnabled ? '6rem' : '20rem');
+  };
+
+  const sideBarStyle = {
+    width: sideBarEnabled ? 'var(--side-bar-size)' : 'var(--side-bar-size)',
+  };
+
   return (
-    <section className="side-nav">
-      <div id="menu-button" className="side-nav-selector">
+    <section className="side-nav" style={sideBarStyle}>
+      <div id="menu-button" className="side-nav-selector" onClick={toggleSideBar}>
         <img src="/icons/brown-icon-ham.png" alt="Ham menu" />
       </div>
-      <div id="menu" className="side-nav-headers hidden">
+      <div id="menu" className={`side-nav-headers ${sideBarEnabled ? 'show' : 'hidden'}`}>
         <ul>
           <li>
             <a href="#about-me-anchor">About Me</a>
