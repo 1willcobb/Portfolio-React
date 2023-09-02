277a2122de39fbc111b337b85ccc750c5b2cdf7c
1willcobb <cobb.will@gmail.com>
2023-09-01 23:36:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/Portfolio-React/'
+    config.base = '/1willcobb.com/'
   }
 
   return config
