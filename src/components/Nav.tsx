45e07ac3fdd29e69c23a15c2243c393d2a64ada1
1willcobb <cobb.will@gmail.com>
2023-09-02 10:39:16 -0500

    styled components and hash links working

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,63 @@
 import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+import { HashLink } from "react-router-hash-link";
 
+const Logo = styled.h1`
+  padding-right: 1rem;
+  white-space: nowrap;
+`;
 
+const NavUL = styled.ul`
+  list-style: none;
+  display: flex;
+  flex: 1 0 0;
+  justify-content: flex-end;
+  padding: 10px 10px;
+  text-decoration: none;
+  white-space: nowrap;
+  transition: width 0.5s ease;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex: 1 0 0;
+  align-items: center;
+  width: 100%;
+  height: var(--top-nav-size);
+  z-index: 1;
+  border-bottom: var(--borders);
+  overflow: auto;
+  padding-left: 4rem;
+  text-shadow: var(--text-pop-subtle);
+  transition: margin 0.5s ease;
+`;
 
 const Nav = () => {
   return (
     <header>
-      <div className="header-container">
-        <h1>Will Cobb</h1>
-        <ul>
+      <HeaderContainer>
+        <Logo>Will Cobb</Logo>
+        <NavUL>
           <li>
-            <NavLink className="nav" to="#about-me-anchor">
+            <HashLink className="nav" to="/pathLink#about-me-anchor">
               About Me
-            </NavLink>
+            </HashLink>
           </li>
           <li>
-            <NavLink className="nav" to="#portfolio-anchor">
+            <HashLink className="nav" to="/pathLink#portfolio-anchor">
               Portfolio
-            </NavLink>
+            </HashLink>
           </li>
           <li>
-            <NavLink className="nav" to="#resume-anchor">
+            <HashLink className="nav" to="/pathLink#resume-anchor">
               Resume
-            </NavLink>
+            </HashLink>
           </li>
-          <li>
-            <NavLink className="nav-btn" to="mailto:cobb.will@gmail.com">
-              Contact Me
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+        </NavUL>
+        <NavLink className="nav-btn" to="mailto:cobb.will@gmail.com">
+          Contact Me
+        </NavLink>
+      </HeaderContainer>
     </header>
   );
 };
